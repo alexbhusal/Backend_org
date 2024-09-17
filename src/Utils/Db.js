3df57fc3e18e0de0fc0d2b4afc32399e089630e6
Alex Bhusal <alexbhusal@Alexs-MacBook-Air.local>
2024-09-17 17:18:25 +0545

    db

--- src/Utils/Db.js
+++ src/Utils/Db.js
@@ -4,13 +4,7 @@ dotenv.config()
 
 
 // make a connection to the database
-
 const connection = mysql.createConnection({
-  // host: "localhost",
-  // user: "root",
-  // password: "",
-  // database: "organization"
-
   host: process.env.Host,
   user: process.env.User,
   password: process.env.Password,
