3dad4a72360d01ac4bca8e41d7cd7c1d15ab954b
Alex Bhusal <alexbhusal@Alexs-MacBook-Air.local>
2024-09-17 16:48:55 +0545

    env

--- src/Utils/Db.js
+++ src/Utils/Db.js
@@ -1,12 +1,21 @@
 const mysql = require('mysql');
+const dotenv = require('dotenv')
+dotenv.config()
+
 
 // make a connection to the database
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'organization',
+  // host: "localhost",
+  // user: "root",
+  // password: "",
+  // database: "organization"
+
+  host: process.env.Host,
+  user: process.env.User,
+  password: process.env.Password,
+  database: process.env.Database
+
 });
 
 module.exports = connection;
