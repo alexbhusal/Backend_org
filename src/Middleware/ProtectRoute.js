7c6baa7fce44aee81c0476ac16ac7b72eb1b8132
Alex Bhusal <145799782+alexbhusal@users.noreply.github.com>
2024-09-18 07:57:01 +0545

    Update ProtectRoute.js

--- src/Middleware/ProtectRoute.js
+++ src/Middleware/ProtectRoute.js
@@ -6,7 +6,7 @@ const { getAdminById } = require('../Utils/Queries');
 module.exports = CatchAsync(async function (req, res, next) {
   const cookie = req?.headers?.cookie;
   const token = cookie?.split('=')?.[1];
-  if (!token) return next(new AppError('Please login to continue'));
+  if (!token) return next(new AppError('Please login to continue',401));
   const decoded = verifyJWTtoken(token);
   const admin_info = await getAdminById(decoded.id);
   if (!admin_info?.length)
